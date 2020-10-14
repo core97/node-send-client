2bc7a875a65dda63483179a644f77b4808e13be6
Usuario <jcortesg1997@gmail.com>
2020-10-14 13:28:21 +0200

    Configuration tailwindcss

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export const Layout = ({ children }) => {
+    return(
+        <>
+        <Head>
+            
+        </Head>
+            {children}
+        </>
+    );
+}
